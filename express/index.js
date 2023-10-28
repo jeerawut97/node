bf92cf76988003c43829ca3b0c25d11c63eb4969
JRW <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1@github.com>
2023-10-28 14:15:20 +0700

    add endpoint & install nodemon

--- express/index.js
+++ express/index.js
@@ -2,6 +2,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello, World!</h1>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About Page</h1>");
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
